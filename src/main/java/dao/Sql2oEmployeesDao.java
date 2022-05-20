8ac20acbb2c19a4c2f6208883d56548f159daef5
mansamusah254 <mansamusah830@gmail.com>
2022-05-20 14:10:12 +0300

    added exception catch

--- src/main/java/dao/Sql2oEmployeesDao.java
+++ src/main/java/dao/Sql2oEmployeesDao.java
@@ -27,6 +27,8 @@ public class Sql2oEmployeesDao implements EmployeesDao {
             employees.setId(id);
         }catch (Sql2oException ex){
             System.out.println(ex);
+        }catch (ClassCastException ex){
+            System.out.println(ex);
         }
     }
 
